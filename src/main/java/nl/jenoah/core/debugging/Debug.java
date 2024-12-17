cec69d81b3febbc3fa06815f40bbedd59106c4a4
Jenoah Kers <hello@jenoah.nl>
2024-12-17 15:04:32 +0100

    Added class and line number to Debug.Log

--- src/main/java/nl/jenoah/core/debugging/Debug.java
+++ src/main/java/nl/jenoah/core/debugging/Debug.java
@@ -3,6 +3,15 @@ package nl.jenoah.core.debugging;
 public class Debug {
 
     public static void Log(String message){
-        System.out.println("\u001B[35m" + "[Log] " + "\u001B[0m" + message);
+        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
+
+        // Find the caller's class and line number (index 2 is the caller of Log())
+        StackTraceElement caller = stackTrace[2];
+
+        // Extract the class name and line number
+        String className = caller.getClassName();
+        int lineNumber = caller.getLineNumber();
+
+        System.out.println("\u001B[35m" + "[Log] " + "\u001B[90m" + className + ":" + lineNumber + " \u001B[0m" + message);
     }
 }
