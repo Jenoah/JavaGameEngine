0f82647ca894b8915f325f78a6d87a1adef1aac0
Jenoah Kers <hello@jenoah.nl>
2025-04-21 23:00:10 +0200

    Made entity generation accept quaternions

--- src/main/java/nl/jenoah/core/entity/Entity.java
+++ src/main/java/nl/jenoah/core/entity/Entity.java
@@ -1,18 +1,48 @@
 package nl.jenoah.core.entity;
 
+import org.joml.Quaternionf;
 import org.joml.Vector3f;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 public class Entity extends GameObject{
 
     private final Model model;
     private boolean transparent = false;
-    private boolean isEnabled = true;
+
 
     public Entity(Model model, Vector3f position, Vector3f rotation, float scale) {
         this.model = model;
         setPosition(position);
         setRotation(rotation);
         setScale(scale);
+    }
+
+    public Entity(Model model, Vector3f position, Quaternionf rotation, float scale) {
+        this.model = model;
+        setPosition(position);
+        setRotation(rotation);
+        setScale(scale);
+    }
+
+    public Entity(Entity entity) {
+        this(entity.getModel(), entity.getPosition(), entity.getEulerAngles(), entity.getScale());
+        transparent = entity.transparent;
+        isEnabled = entity.isEnabled;
+    }
+
+    public Entity(Entity entity, Vector3f position, Vector3f rotation, float scale) {
+        this(entity.getModel(), position, rotation, scale);
+        transparent = entity.transparent;
+        isEnabled = entity.isEnabled;
+    }
+
+    public Entity(Entity entity, Vector3f position, Quaternionf rotation, float scale) {
+        this(entity.getModel(), position, rotation, scale);
+        transparent = entity.transparent;
+        isEnabled = entity.isEnabled;
     }
 
     public Entity(Model model, Vector3f position, Vector3f rotation, Vector3f scale) {
@@ -37,13 +67,5 @@ public class Entity extends GameObject{
 
     public boolean isTransparent(){
         return transparent;
-    }
-
-    public boolean isEnabled() {
-        return isEnabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        isEnabled = enabled;
     }
 }
