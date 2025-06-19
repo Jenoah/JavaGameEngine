e84fa5c72f19a9937d41b469edf3beed17d92e9b
Jenoah Kers <hello@jenoah.nl>
2025-06-19 23:16:42 +0200

    Moved player movement to separate component

---
+++ src/main/java/game/components/PlayerMovement.java
@@ -0,0 +1,97 @@
+package game.components;
+
+import nl.jenoah.core.EngineManager;
+import nl.jenoah.core.MouseInput;
+import nl.jenoah.core.WindowManager;
+import nl.jenoah.core.components.Component;
+import nl.jenoah.core.entity.GameObject;
+import nl.jenoah.core.utils.Constants;
+import nl.jenoah.core.utils.ObjectPool;
+import org.joml.Quaternionf;
+import org.joml.Vector2f;
+import org.joml.Vector3f;
+import org.lwjgl.glfw.GLFW;
+
+public class PlayerMovement extends Component{
+    private final WindowManager windowManager;
+    private final GameObject cameraObject;
+
+    private final Vector3f moveDelta = new Vector3f();
+    private float pitch = 0;
+    private float yaw = 0;
+
+    public PlayerMovement(GameObject cameraObject) {
+        this.windowManager = WindowManager.getInstance();
+        this.cameraObject = cameraObject;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+
+        move();
+
+    }
+
+    public void input(MouseInput mouseInput){
+        if(!mouseInput.isRbDown()) return;
+        rotate(mouseInput.getMouseDelta());
+    }
+
+    private void move(){
+        moveDelta.set(0, 0, 0);
+        float moveSpeed = windowManager.isKeyPressed(GLFW.GLFW_KEY_LEFT_SHIFT) ?
+                Constants.CAMERA_MOVE_SPEED * EngineManager.getDeltaTime() * 4f :
+                Constants.CAMERA_MOVE_SPEED * EngineManager.getDeltaTime();
+
+        //Forwards / Backwards
+        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_W)){
+            moveDelta.z = -1;
+        }else if(windowManager.isKeyPressed(GLFW.GLFW_KEY_S)){
+            moveDelta.z = 1;
+        }
+        //Left / right
+        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_A)){
+            moveDelta.x = -1;
+        } else if(windowManager.isKeyPressed(GLFW.GLFW_KEY_D)){
+            moveDelta.x = 1;
+        }
+        //Up / down
+        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_SPACE) || windowManager.isKeyPressed(GLFW.GLFW_KEY_E)){
+            moveDelta.y = 1;
+        }else if(windowManager.isKeyPressed(GLFW.GLFW_KEY_LEFT_CONTROL) || windowManager.isKeyPressed(GLFW.GLFW_KEY_Q)){
+            moveDelta.y = -1;
+        }
+
+        if(moveDelta.length() > 0) moveDelta.normalize(moveSpeed);
+
+        Vector3f targetPosition = ObjectPool.VECTOR3F_POOL.obtain().set(0);
+
+        if(moveDelta.z != 0){
+            targetPosition.x += (float) Math.sin(yaw) * -1f * moveDelta.z;
+            targetPosition.z += (float) Math.cos(yaw) * moveDelta.z;
+        }
+
+        if(moveDelta.x != 0){
+            targetPosition.x += (float) Math.sin((yaw - Constants.DEGREES_90_IN_RADIANS)) * -1f * moveDelta.x;
+            targetPosition.z += (float) Math.cos((yaw - Constants.DEGREES_90_IN_RADIANS)) * moveDelta.x;
+        }
+
+        targetPosition.y += moveDelta.y;
+
+        root.translateLocal(targetPosition);
+    }
+
+    public void rotate(Vector2f mouseDelta){
+        pitch += mouseDelta.x * Constants.MOUSE_SENSITIVITY * EngineManager.getDeltaTime();
+        yaw += mouseDelta.y * Constants.MOUSE_SENSITIVITY * EngineManager.getDeltaTime();
+
+        pitch = Math.clamp(pitch, -Constants.DEGREES_90_IN_RADIANS, Constants.DEGREES_90_IN_RADIANS);
+
+        Quaternionf targetRotation = ObjectPool.QUATERNIONF_OBJECT_POOL.obtain().identity().rotateX(pitch).rotateY(yaw).normalize();
+
+        cameraObject.setRotation(targetRotation);
+
+        ObjectPool.QUATERNIONF_OBJECT_POOL.free(targetRotation);
+    }
+}
--- src/main/java/game/entities/Player.java
+++ src/main/java/game/entities/Player.java
@@ -1,36 +1,32 @@
 package game.entities;
 
+import game.components.PlayerMovement;
 import nl.jenoah.core.*;
 import nl.jenoah.core.components.RenderComponent;
 import nl.jenoah.core.entity.*;
 import nl.jenoah.core.loaders.PrimitiveLoader;
 import nl.jenoah.core.shaders.ShaderManager;
-import nl.jenoah.core.utils.Constants;
-import org.joml.Quaternionf;
-import org.joml.Vector2f;
 import org.joml.Vector3f;
 import org.joml.Vector4f;
-import org.lwjgl.glfw.GLFW;
 
 public class Player extends GameObject {
 
     private final Camera camera;
-    private final WindowManager windowManager;
 
-    private final Vector3f moveDelta;
-    private float pitch = 0;
-    private float yaw = 0;
-    private float moveSpeed = Constants.CAMERA_MOVE_SPEED;
+    private final PlayerMovement playerMovement;
 
     public Player(){
-        this.windowManager = WindowManager.getInstance();
         this.camera = new Camera();
         this.camera.setParent(this);
-        moveDelta = new Vector3f(0, 0, 0);
 
         GameObject feet = new GameObject().setPosition(new Vector3f(0, -1f, 0)).setScale(0.1f);
         Material feetMaterial = new Material(ShaderManager.unlitShader).setAmbientColor(new Vector4f(1, .5f, 1, 1));
         feet.addComponent(new RenderComponent(PrimitiveLoader.getCube().getMesh(), feetMaterial));
+
+        playerMovement = new PlayerMovement(this.camera);
+
+        addComponent(playerMovement);
+        playerMovement.initiate();
 
         feet.setParent(this);
     }
@@ -38,77 +34,10 @@ public class Player extends GameObject {
     @Override
     public void update(MouseInput mouseInput) {
         super.update(mouseInput);
-        input();
-        move();
-        rotate(mouseInput);
-    }
+        playerMovement.input(mouseInput);
+        playerMovement.update();
 
-    private void input(){
-        moveDelta.set(0, 0, 0);
-
-        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_W)){
-            moveDelta.z = -1;
-        }
-        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_S)){
-            moveDelta.z = 1;
-        }
-        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_A)){
-            moveDelta.x = -1;
-        }
-        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_D)){
-            moveDelta.x = 1;
-        }
-        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_SPACE) || windowManager.isKeyPressed(GLFW.GLFW_KEY_E)){
-            moveDelta.y = 1;
-        }
-        if(windowManager.isKeyPressed(GLFW.GLFW_KEY_LEFT_CONTROL) || windowManager.isKeyPressed(GLFW.GLFW_KEY_Q)){
-            moveDelta.y = -1;
-        }
-        moveSpeed = windowManager.isKeyPressed(GLFW.GLFW_KEY_LEFT_SHIFT) ? Constants.CAMERA_MOVE_SPEED * 4f * EngineManager.getDeltaTime() : Constants.CAMERA_MOVE_SPEED * EngineManager.getDeltaTime();
-    }
-
-    private void move(){
-        Vector3f moveDirection = new Vector3f(moveDelta.x * moveSpeed, moveDelta.y * moveSpeed, moveDelta.z * moveSpeed);
-        movePosition(moveDirection);
-    }
-
-    private void rotate(MouseInput mouseInput){
-        if(mouseInput.isRbDown()){
-            Vector2f rotationVector = mouseInput.getMouseDelta();
-            moveRotation(rotationVector.x * Constants.MOUSE_SENSITIVITY * EngineManager.getDeltaTime(), rotationVector.y * Constants.MOUSE_SENSITIVITY * EngineManager.getDeltaTime());
-            //mouseInput.hide();
-        } else {
-            // Show the cursor when the right button is not pressed
-            //mouseInput.show();
-        }
-    }
-
-    public void movePosition(Vector3f movePosition){
-        Vector3f newPosition = new Vector3f(getPosition());
-
-        if(movePosition.z != 0){
-            newPosition.x += (float) Math.sin(yaw) * -1f * movePosition.z;
-            newPosition.z += (float) Math.cos(yaw) * movePosition.z;
-        }
-
-        if(movePosition.x != 0){
-            newPosition.x += (float) Math.sin((yaw - Constants.DEGREES_90_IN_RADIANS)) * -1f * movePosition.x;
-            newPosition.z += (float) Math.cos((yaw - Constants.DEGREES_90_IN_RADIANS)) * movePosition.x;
-        }
-
-        newPosition.y += movePosition.y;
-        setPosition(newPosition);
-    }
-
-    public void moveRotation(float xDelta, float yDelta){
-        pitch += xDelta;
-        yaw += yDelta;
-
-        pitch = Math.clamp(pitch, -Constants.DEGREES_90_IN_RADIANS, Constants.DEGREES_90_IN_RADIANS);
-
-        Quaternionf targetRotation = new Quaternionf().rotateX(pitch).rotateY(yaw).normalize();
-
-        camera.setRotation(targetRotation);
+        camera.updateViewFrustum();
     }
 
     public Camera getCamera(){
