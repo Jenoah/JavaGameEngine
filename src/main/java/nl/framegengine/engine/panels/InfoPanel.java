cc14149766f90b4e80190e1918204c7f8ceb9e48
Jenoah Kers <hello@jenoah.nl>
2025-07-15 23:10:56 +0200

    Added properties of selected object in Engine InfoPanel

--- src/main/java/nl/framegengine/engine/panels/InfoPanel.java
+++ src/main/java/nl/framegengine/engine/panels/InfoPanel.java
@@ -4,6 +4,11 @@ import imgui.ImGui;
 import imgui.flag.ImGuiCol;
 import nl.framegengine.engine.EditorPanel;
 import nl.jenoah.core.entity.GameObject;
+import nl.jenoah.core.utils.Utils;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
 
 public class InfoPanel extends EditorPanel {
 
@@ -21,8 +26,19 @@ public class InfoPanel extends EditorPanel {
         ImGui.text(currentlySelectedObject.getName());
         ImGui.newLine();
 
-        drawOption("Position", currentlySelectedObject.getPosition().toString());
-        drawOption("Rotation", currentlySelectedObject.getEulerAngles().toString());
+        List<Field> instanceFields = new ArrayList<>();
+        Utils.getAllProperties(instanceFields, currentlySelectedObject.getClass());
+        for (Field field : instanceFields) {
+            try {
+                field.setAccessible(true);
+                Object value = field.get(currentlySelectedObject);
+                if(value == null) continue;
+                drawOption(field.getName(), value.toString());
+            } catch (IllegalAccessException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
 
         ImGui.pushStyleColor(ImGuiCol.ButtonHovered, 1f, 1f, 0f, 1f);
         ImGui.pushStyleColor(ImGuiCol.Text, .2f, .2f, .2f, 1f);
@@ -49,6 +65,8 @@ public class InfoPanel extends EditorPanel {
         ImGui.text(title);
         ImGui.setWindowFontScale(1.1f);
         ImGui.text(content);
+        ImGui.setWindowFontScale(0.4f);
         ImGui.newLine();
+        ImGui.setWindowFontScale(1f);
     }
 }
