566bcd1ed1709b1733f863585402f5e1fa21b535
Jenoah Kers <hello@jenoah.nl>
2025-07-15 23:06:46 +0200

    Added empty constructor for DirectionalLight

--- src/main/java/nl/jenoah/core/lighting/DirectionalLight.java
+++ src/main/java/nl/jenoah/core/lighting/DirectionalLight.java
@@ -5,6 +5,8 @@ import org.joml.Vector3f;
 
 public class DirectionalLight extends Light{
 
+    public DirectionalLight(){ super(); }
+
     public DirectionalLight(Vector3f color, Vector3f direction, float intensity) {
         super(color, new Vector3f(0), intensity, 0);
         lookAtDirection(direction);
